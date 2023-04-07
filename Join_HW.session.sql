5cd54df954d8f6eae2720b9204b254aeec8cfe10
Brian Vo <Vobrian025@gmail.com>
2023-04-07 13:50:55 -0400

    week4day3

--- Join_HW.session.sql
+++ Join_HW.session.sql
@@ -3,7 +3,6 @@ SELECT first_name, last_name
 FROM customer
 
 
-
 --2. Get all payments above $6.99 with the Customer's Full Name 
 SELECT first_name, last_name
 FROM customer, payment
@@ -19,20 +18,25 @@ WHERE amount > 175
 );
 
 
---4. List all customers that live in Nepal (use the city table)
-SELECT *
-FROM city
-LEFT JOIN customer
+--4. List all customers that live in Nepal (use the city table) 524 
+--Did not use city table because Nepal is a country unless I did it wrong?
+SELECT customer_id, country
+FROM customer, country;
 
---5. Which staff member had the most transactions?
-SELECT staff, amount
-FROM staff, payment;
---6. How many movies of each rating are there?
+
+--5. Which staff member had the most transactions? Had problems with this tried inner join but didn't get it
+SELECT s.staff_id, payment.payment_id
+FROM staff AS s
+INNER JOIN payment_id AS payment
+ON s.staff_id = p.payment_id;
+
+--6. How many movies of each rating are there? a lot
 SELECT rating, film_id
 FROM film;
 
 --7.Show all customers who have made a single payment above $6.99 (Use Subqueries)
--- Mary, Peter, Douglas, Alvin, Alfredo
+-- Mary, Peter, Douglas, Alvin, Alfredo. I did the same thing here for #3 too but changed the amount
+-- not sure if I did it right?
 SELECT first_name, customer.customer_id
 FROM customer
 WHERE customer_id IN(
@@ -41,6 +45,8 @@ FROM payment
 WHERE amount > 6.99
 );
 
---8.)How many free rentals did out store give away?
+--8.)How many free rentals did out store give away? Touble with this problem too Ambiguous
+SELECT payment.amount, rental.rental_id
+FROM payment, rental
+WHERE amount = 0;
 
---I will resubmit when finished.
