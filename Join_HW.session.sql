a2d769e42538808234c6ce19e65ffc014a70e0bc
Brian Vo <Vobrian025@gmail.com>
2023-05-17 17:36:32 -0400

    SQLsucks

--- Join_HW.session.sql
+++ Join_HW.session.sql
@@ -1,52 +1,79 @@
---1. List all customers who live in Texas (use JOINS)
-SELECT first_name, last_name
+--1. List all customers who live in Texas (use JOINS) 5
+SELECT first_name, address.district
 FROM customer
+INNER JOIN address
+ON address.address_id = customer.address_id
+WHERE address.district = 'Texas';
 
 
 --2. Get all payments above $6.99 with the Customer's Full Name 
-SELECT first_name, last_name
-FROM customer, payment
+SELECT CONCAT(first_name,' ',last_name) as full_name, payment.amount
+FROM customer
+JOIN payment
+ON customer.customer_id = payment.customer_id
 WHERE payment.amount > 6.99;
 
 --3. Show all customers names who have made payments over $175(use subqueries) 2 Douglas and Mary
-SELECT first_name, customer.customer_id
+SELECT first_name, payment.amount
 FROM customer
-WHERE customer_id IN(
-SELECT customer_id
+JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE payment_id IN(
+SELECT payment_id
 FROM payment
 WHERE amount > 175
 );
 
-
---4. List all customers that live in Nepal (use the city table) 524 
---Did not use city table because Nepal is a country unless I did it wrong?
-SELECT customer_id, country
-FROM customer, country;
-
-
---5. Which staff member had the most transactions? Had problems with this tried inner join but didn't get it
-SELECT s.staff_id, payment.payment_id
-FROM staff AS s
-INNER JOIN payment_id AS payment
-ON s.staff_id = p.payment_id;
-
---6. How many movies of each rating are there? a lot
-SELECT rating, film_id
-FROM film;
-
---7.Show all customers who have made a single payment above $6.99 (Use Subqueries)
--- Mary, Peter, Douglas, Alvin, Alfredo. I did the same thing here for #3 too but changed the amount
--- not sure if I did it right?
-SELECT first_name, customer.customer_id
+--4. List all customers that live in Nepal (use the city table) Kevin
+SELECT first_name, country  
 FROM customer
-WHERE customer_id IN(
-SELECT customer_id
+JOIN address
+ON customer.address_id = address.address_id
+JOIN city
+ON city.city_id = address.city_id
+JOIN country
+ON city.country_id = country.country_id
+WHERE country = 'Nepal';
+
+
+--5. Which staff member had the most transactions? JON
+SELECT first_name, staff.staff_id, COUNT(payment_id)
+FROM staff
+JOIN payment
+ON staff.staff_id = payment.staff_id
+GROUP BY first_name, staff.staff_id
+ORDER BY COUNT(payment_id) DESC;
+
+
+--6. How many movies of each rating are there?
+SELECT rating, COUNT(*) AS rating_count
+FROM film
+GROUP BY rating;
+
+
+--7.Show all customers who have made a single payment above $6.99 (Use Subqueries) 5 customers
+SELECT first_name, payment.amount
+FROM customer
+JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE payment_id IN(
+SELECT payment_id
 FROM payment
 WHERE amount > 6.99
 );
 
---8.)How many free rentals did out store give away? Touble with this problem too Ambiguous
-SELECT payment.amount, rental.rental_id
-FROM payment, rental
-WHERE amount = 0;
+--8.)How many free rentals did out store give away?
+SELECT rental_id, payment.amount
+FROM rental
+JOIN payment
+ON rental.rental_id = payment.rental_id
+WHERE payment_id IN (
+SELECT payment_id
+FROM payment
+WHERE amount = 0
+);
+
+
+
+
 
